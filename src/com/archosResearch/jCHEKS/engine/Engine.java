a5edd827f0d6b6ad2849567f69b721fa26770e49
LepageT <thomas.lepage@hotmail.ca>
2015-06-30 12:35:17 -0400

    Adapted the engine to the new encryption methods.

--- src/com/archosResearch/jCHEKS/engine/Engine.java
+++ src/com/archosResearch/jCHEKS/engine/Engine.java
@@ -10,6 +10,7 @@ import com.archosResearch.jCHEKS.engine.model.contact.Contact;
 import com.archosResearch.jCHEKS.engine.model.contact.exception.ContactAlreadyExistException;
 import com.archosResearch.jCHEKS.gui.chat.view.JavaFxViewController;
 import com.archosResearch.jCHEKS.concept.communicator.*;
+import com.archosResearch.jCHEKS.concept.encrypter.AbstractEncrypter;
 import com.archosResearch.jCHEKS.concept.engine.message.*;
 import com.archosResearch.jCHEKS.concept.exception.CommunicatorException;
 import com.archosResearch.jCHEKS.concept.exception.EncrypterException;
@@ -17,7 +18,9 @@ import com.archosResearch.jCHEKS.concept.ioManager.*;
 import com.archosResearch.jCHEKS.encrypter.RijndaelEncrypter;
 import com.archosResearch.jCHEKS.engine.model.contact.exception.ContactNotFoundException;
 import com.archosResearch.jCHEKS.messageChecker.MessageChecker;
+import com.archosResearch.jCHEKS.messageChecker.SecureAckGenerator;
 import com.archosResearch.jCHEKS.messageChecker.exception.MessageCheckerException;
+import com.archosResearch.jCHEKS.messageChecker.exception.SecureAckGeneratorException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.logging.*;
@@ -54,15 +57,27 @@ public class Engine extends AbstractEngine  implements CommunicatorObserver{
     }
     
     @Override
-    public void secureAckReceived(AbstractCommunication communication) {
+    public void secureAckReceived(AbstractCommunication communication, String secureAck) {
         try {
             String systemId = communication.getSystemId();
-            OutgoingMessage message = this.model.getLastOutgoingMessageBySystemId(systemId);
-            message.updateState(AbstractMessage.State.OK);
             Contact contact = this.model.findContactByReceiverSystemId(systemId);
-            this.ioManager.refresh();
-            this.ioManager.log("Secure acknowledge received.", contact.getContactInfo().getName());
-            contact.getSendingChaoticSystem().evolveSystem();
+            AbstractChaoticSystem chaoticSystem = contact.getSendingChaoticSystem();
+            
+            byte[] secureAckKey = chaoticSystem.getKey();
+            
+            try {
+                if(SecureAckGenerator.validateSecureAck(secureAckKey, secureAck, communication.getCipherCheck())) {
+                    OutgoingMessage message = this.model.getLastOutgoingMessageBySystemId(systemId);
+                    message.updateState(AbstractMessage.State.OK);
+                    this.ioManager.refresh();
+                    this.ioManager.log("Secure acknowledge received.", contact.getContactInfo().getName());
+                    contact.getSendingChaoticSystem().evolveSystem();
+                } else {
+                    this.ioManager.log("Wrong secure ack received", contact.getContactInfo().getName());     
+                }
+            } catch (SecureAckGeneratorException ex) {
+                    this.ioManager.log("Error while validating the secure ack.", contact.getContactInfo().getName());     
+            }
         } catch (ContactNotFoundException ex) {
             Logger.getLogger(Engine.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -73,16 +88,25 @@ public class Engine extends AbstractEngine  implements CommunicatorObserver{
         try {
             Contact contact = this.model.findContactByReceiverSystemId(communication.getSystemId());
             AbstractChaoticSystem chaoticSystem = contact.getReceivingChaoticSystem();
-            byte[] key = chaoticSystem.getKey();
-            byte[] iv = chaoticSystem.getIV();
-            String decryptedMessage = contact.getEncrypter().decrypt(communication.getCipher(), key, iv);
-            this.ioManager.log("Communication received: \n      Key: " + Arrays.toString(key)+" \n      IV" + Arrays.toString(iv), contact.getContactInfo().getName());
+            AbstractEncrypter encrypter = contact.getEncrypter();
             
-            try {          
-                if(MessageChecker.validateMessage(key, decryptedMessage, communication.getCipherCheck())) {
-                    this.model.addIncomingMessage(decryptedMessage, contact); 
-                    chaoticSystem.evolveSystem();
-                    return communication.getCipherCheck();
+            byte[] key = chaoticSystem.getKey(encrypter.bytesNeeded());
+            String decryptedMessage = encrypter.decrypt(communication.getCipher(), key);
+            this.ioManager.log("Communication received: \n      Key: " + Arrays.toString(key), contact.getContactInfo().getName());
+            
+            try { 
+                byte[] checkKey = chaoticSystem.getKey();
+                if(MessageChecker.validateMessage(checkKey, decryptedMessage, communication.getCipherCheck())) {
+                    this.model.addIncomingMessage(decryptedMessage, contact);
+                    byte[] secureAckKey = chaoticSystem.getKey();
+                    try {
+                        String secureAck = SecureAckGenerator.generateSecureAck(secureAckKey, communication.getCipher());
+                        chaoticSystem.evolveSystem();
+                        return secureAck;
+                    } catch (SecureAckGeneratorException ex) {
+                        this.ioManager.log("Error: " + ex.getMessage(), contact.getContactInfo().getName());
+                    }
+                    
                 }
             } catch (MessageCheckerException ex) {
                 this.ioManager.log("Error while checking the cipher check: " + ex.getMessage(), contact.getContactInfo().getName());
@@ -91,6 +115,8 @@ public class Engine extends AbstractEngine  implements CommunicatorObserver{
             //TODO return something else.
             return "No secure ACK";
         } catch (ContactNotFoundException | EncrypterException ex) {
+            Logger.getLogger(Engine.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
             Logger.getLogger(Engine.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
@@ -169,10 +195,11 @@ public class Engine extends AbstractEngine  implements CommunicatorObserver{
             
             Contact contact = this.model.findContactByName(contactName);
             AbstractChaoticSystem chaoticSystem = contact.getSendingChaoticSystem();
-            byte[] key = chaoticSystem.getKey();
-            byte[] iv = chaoticSystem.getIV();
+            AbstractEncrypter encrypter = contact.getEncrypter();
             
-            String encryptedMessage = contact.getEncrypter().encrypt(messageContent, key, iv);
+            byte[] key = chaoticSystem.getKey(encrypter.bytesNeeded());   
+            
+            String encryptedMessage = encrypter.encrypt(messageContent, key);
             String cipherCheck;
             try {
                 cipherCheck = MessageChecker.encodeMessage(key, messageContent);
@@ -182,6 +209,8 @@ public class Engine extends AbstractEngine  implements CommunicatorObserver{
                 Logger.getLogger(Engine.class.getName()).log(Level.SEVERE, null, ex);
             }
         } catch ( CommunicatorException | ContactNotFoundException | EncrypterException ex) {
+            Logger.getLogger(Engine.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
             Logger.getLogger(Engine.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
